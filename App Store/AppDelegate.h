678bd5218a8ca5f0fa76c2fbd0ced02634a97b51
Antique <official.antique@gmail.com>
2019-01-19 16:41:12 +0800

    Initial App Commit

---
+++ App Store/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Design
+//
+//  Created by Antique on 16/11/18.
+//  Copyright © 2018 Antique. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
